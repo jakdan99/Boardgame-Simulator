08f3153bf4d8e6ed4ab82bd166f2de3a9bdac58b
Jakab Dániel <jakab.daniel@autsoft.hu>
2021-10-15 16:31:34 +0200

    Inventory remove

---
+++ BoardgameSimulator.Unity/Assets/BoardgameSimulator.Unity/Inventory/Inventory.cs
@@ -0,0 +1,40 @@
+using Microsoft.MixedReality.Toolkit.UI;
+using Microsoft.MixedReality.Toolkit.Utilities;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BoardgameSimulator.Unity.HandMenus
+{
+    public class Inventory : MonoBehaviour
+    {
+        [SerializeField] private GameObject _storageSpace = default!;
+        private readonly Dictionary<GameObject, GameObject> _storedItems = new Dictionary<GameObject, GameObject>();
+
+        private void Awake()
+        {
+            if (_storageSpace == null) throw new InvalidOperationException("Storage Space should not be null");
+        }
+
+        private void OnCollisionEnter(Collision other)
+        {
+            if (other.gameObject.TryGetComponent(out Storable storable))
+            {
+                var storage = Instantiate(_storageSpace, GetComponentInChildren<GridObjectCollection>().transform, false);
+                storage.GetComponent<Interactable>().OnClick.AddListener(delegate { RemoveItem(storage); });
+                storable.PlaceInInventory(storage);
+
+                _storedItems.Add(storage, other.gameObject);
+
+                GetComponentInChildren<GridObjectCollection>().UpdateCollection();
+            }
+        }
+
+        private void RemoveItem(GameObject item)
+        {
+            _storedItems[item].GetComponent<Storable>().RemoveFromInventory();
+            Destroy(item);
+            GetComponentInChildren<GridObjectCollection>().UpdateCollection();
+        }
+    }
+}
---
+++ BoardgameSimulator.Unity/Assets/BoardgameSimulator.Unity/Inventory/Storable.cs
@@ -0,0 +1,41 @@
+using Microsoft.MixedReality.Toolkit;
+using Microsoft.MixedReality.Toolkit.UI;
+using System;
+using UnityEngine;
+
+public class Storable : MonoBehaviour
+{
+    [SerializeField] private Transform _sceneContentTransform = default!;
+
+    private Vector3 _originalScale;
+
+    private void Awake()
+    {
+        if (_sceneContentTransform == null) throw new InvalidOperationException("SceneContentTransform should not be null");
+    }
+
+    public void PlaceInInventory(GameObject parent)
+    {
+        _originalScale = transform.localScale;
+
+        Destroy(GetComponent<Rigidbody>());
+        Destroy(GetComponent<ObjectManipulator>());
+
+        transform.SetParent(parent.transform, false);
+
+        var scaleToFit = GetComponent<Collider>().bounds.GetScaleToFitInside(parent.GetComponent<Collider>().bounds);
+        transform.localScale *= scaleToFit * 0.25f;
+        transform.localRotation = Quaternion.Euler(Vector3.zero);
+        transform.localPosition = Vector3.zero;
+        parent.GetComponent<ButtonConfigHelper>().MainLabelText = gameObject.name;
+    }
+
+    public void RemoveFromInventory()
+    {
+        transform.SetParent(_sceneContentTransform.transform, true);
+        transform.localScale = _originalScale;
+        transform.localRotation = Quaternion.Euler(Vector3.zero);
+        gameObject.AddComponent<Rigidbody>().useGravity = false;
+        gameObject.AddComponent<ObjectManipulator>();
+    }
+}
